863982c97d4ef299c34941f3a0af4770d423a59b
MD IMRAN MRIDHA <mdimranmridha.pro@gmail.com>
2022-08-22 13:15:26 +0600

    Updated selected player

---
+++ js/card.js
@@ -0,0 +1,87 @@
+const playerArr = [];
+
+
+function display(addedPlayers){
+  const players = document.getElementById('players');
+  // Empty Player
+  players.innerHTML = '';
+  // Get Allplayer of player
+  for(player of addedPlayers){
+    const li = document.createElement('li');
+    li.innerText = player;
+    li.classList.add('py-1');
+    players.appendChild(li);
+  }
+}
+// Get Player Name When click Select Button with onclick function
+
+function addPlayer(element){
+  element.setAttribute('disabled','true')
+  element.classList.add('bg-red-300')
+  // console.log(element.classList);
+  // Get Player Name
+
+  const playerName = (element.parentNode.parentNode.children[0].children[0].innerText);
+  // console.log(playerName);
+
+  playerArr.push(playerName);
+  // console.log(player);
+
+  display(playerArr);
+}
+
+// Get Input field Value with function
+
+function getInputFieldValue(inputId){
+  const inputFieldValue = document.getElementById(inputId);
+  const inputValueString = inputFieldValue.value;
+  const inputValue = parseFloat(inputValueString);
+
+  return inputValue;
+}
+
+// Get Text Element Value with Function
+
+function getTextElementValue(elementId){
+
+  const textElementValue = document.getElementById(elementId);
+  const elementValueString = textElementValue.innerText;
+  const elementValue = parseFloat(elementValueString);
+
+  return elementValue;
+}
+
+// Set Text Element Field Value On the Text Element with function
+
+function setTextElementValue(elementId, value){
+  const textElementValue = document.getElementById(elementId);
+  textElementValue.innerText = value;
+}
+// Set Click Event Handaler on the Calculate button
+
+document.getElementById('calculate').addEventListener('click', function(){
+
+  // Get Players Length
+
+  const playersLength = playerArr.length;
+
+  // get Per player input value by id
+
+  const perPlayerValue = getInputFieldValue('per-player');
+
+  // Calculate Player expenses
+
+  const totalPlayerExpense = (perPlayerValue * playersLength);
+
+  // Get Player expense Element Value
+
+  const playerExpense = getTextElementValue('player-expense');
+
+  // Set Player Expense Ammount on the Player Expense Element
+
+
+
+  console.log(playerExpense);
+
+
+})
